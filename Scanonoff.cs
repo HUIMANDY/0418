e89e147fd62ae7210a0e1a9ee0e2db0bad5e56cd
HUIMANDY <80736252+HUIMANDY@users.noreply.github.com>
2021-04-19 14:41:15 +0800

    Add files via upload

---
+++ Scanonoff.cs
@@ -0,0 +1,118 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using MRTKExtensions.QRCodes;
+
+public class Scanonoff : MonoBehaviour
+{
+    public GameObject QRcodescan;
+    //public GameObject qrcode;
+    //public GameObject HandMenu;
+    public GameObject JetHolder;
+    public GameObject triggerObj;
+    public Canvas Canvas01;
+    //public GameObject QRcodescan1;
+    //public GameObject TRYT;
+    public bool QRonoff = false;
+    public bool scann = true;
+    public bool Modeldestroy = false;
+    public Text textscan;
+    public Text textt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // qrcode = GameObject.Find("QRCODE");
+        //HandMenu = GameObject.Find("HandMenu");
+        JetHolder = GameObject.Find("JetHolder");
+        triggerObj = GameObject.Find("triggerObj");
+        QRcodescan = GameObject.Find("Trackers");
+        QRcodescan.SetActive(false);
+        //HandMenu.SetActive(false);
+        JetHolder.SetActive(false);
+        //triggerObj.SetActive(false);
+        textt.text = "";
+        textscan.text = "";
+        //qrcode.SetActive(false);
+    }
+    public void scan()
+
+    {
+        textscan.text = "掃描開啟中";
+
+        //QRcodescan1 = GameObject.Find("Tracker2");
+        //TRYT = GameObject.Find("JetHolder");
+        if (scann == true)
+        {
+            QRcodescan.SetActive(false);
+            //QRcodescan1.GetComponent<QRTrackerController>().enabled = false;
+            //TRYT.GetComponent<JetController>().enabled = false;
+            //TRYT.GetComponent<JetController2>().enabled = false;
+            scann = false;
+            textscan.text = "掃描關閉中";
+        }
+        else
+        {
+            QRcodescan.SetActive(true);
+            // QRcodescan1.GetComponent<QRTrackerController>().enabled = true;
+            // TRYT.GetComponent<JetController>().enabled = true;
+            //TRYT.GetComponent<JetController2>().enabled = true;
+            scann = true;
+            textscan.text = "掃描開啟中";
+        }
+    }
+    public void QRONOFF()
+
+    {
+
+        if (QRonoff == true)
+        {
+            QRcodescan.SetActive(false);
+            //HandMenu.SetActive(false);
+            JetHolder.SetActive(false);
+            //triggerObj.SetActive(false);
+            textt.text = "";
+            textscan.text = "";
+            QRonoff = false;
+           // closeviss(QRloading.go1);
+        }
+        else
+        {
+
+            QRcodescan.SetActive(true);
+            //HandMenu.SetActive(true);
+            JetHolder.SetActive(true);
+            //triggerObj.SetActive(true);
+            textt.text = "(0,0,0)";
+            textscan.text = "掃描開啟中";
+            QRonoff = true; ;
+
+        }
+    }
+    public void closeviss(GameObject model)
+    {
+        
+        foreach (Transform child in model.transform)
+        {
+            if (null == child)
+                continue;
+            if (child.gameObject.GetComponent<MeshRenderer>())
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+
+            closeviss(child.gameObject);
+
+        }
+    }
+    public void ModelDestroy()
+    {
+        Destroy(Bundle_Test.go1);
+    }
+
+    // Update is called once per frame
+    void Update()
+        {
+
+        }
+    
+}
---
+++ Set_Model.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Set_Model : MonoBehaviour
+{
+    public static bool Location;
+    public static int Points = 1;
+    public static GameObject PointA;
+    public static GameObject PointB;
+    public static GameObject PointC;
+    public static GameObject PointD;
+    public static GameObject thismdll;
+    private static Vector3 Vba;
+    private static Vector3 Vac;
+    private static Vector3 Vbd;
+    private static Vector3 record;
+    private static float Lac;
+    private static float Lbd;
+    private static float angle;
+    private RaycastHit hitInf;
+    private float distance;
+
+    // Use this for initialization
+    void Awake()
+    {
+        thismdll = GameObject.Find("ModelManager");
+    }
+
+    public static void ModelLocation()
+    {
+
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(
+                Camera.main.transform.position,
+                Camera.main.transform.forward,
+                out hitInfo,
+                20.0f,
+                Physics.DefaultRaycastLayers))
+        {
+
+            if (Points == 1)
+            {
+                Debug.Log("1");
+                PointA = Instantiate(GameObject.Find("LocationManager").transform.GetChild(0).gameObject);
+                PointA.SetActive(true);
+                PointA.transform.position = hitInfo.point;
+                PointA.transform.parent = GameObject.Find("LocationManager").transform;
+                /* foreach (Transform child in thismdll.transform)
+                 {
+
+                     foreach (Transform childin in child.transform)
+                     {
+
+                         if (childin.gameObject.GetComponent<MeshCollider>() == true)
+                         {
+                             childin.gameObject.GetComponent<MeshCollider>().enabled = true;
+                         }
+                     }
+                 }*/
+                openmesh(thismdll);
+                openrenderer(thismdll);
+
+                 Points++;
+                return;
+
+
+
+            }
+            if (Points == 2)
+            {
+                Debug.Log("2");
+                PointB = Instantiate(GameObject.Find("LocationManager").transform.GetChild(0).gameObject);
+                PointB.SetActive(true);
+                PointB.transform.position = hitInfo.point;
+                PointB.transform.parent = GameObject.Find("LocationManager").transform;
+                /*  foreach (Transform child in thismdll.transform)
+                  {
+
+                      foreach (Transform childin in child.transform)
+                      {
+                          if (childin.gameObject.GetComponent<MeshCollider>() == true)
+                          {
+                              childin.gameObject.GetComponent<MeshCollider>().enabled = false;
+                          }
+
+                      }
+                  }*/
+                closemesh(thismdll);
+                closerenderer(thismdll);
+
+                Points++;
+                return;
+
+
+            }
+            if (Points == 3)
+            {
+                Debug.Log("3");
+                PointC = Instantiate(GameObject.Find("LocationManager").transform.GetChild(0).gameObject);
+                PointC.SetActive(true);
+                PointC.transform.position = hitInfo.point;
+                PointC.transform.parent = GameObject.Find("LocationManager").transform;
+
+
+                openmesh(thismdll);
+                openrenderer(thismdll);
+                Points++;
+
+                return;
+            }
+            if (Points == 4)
+            {
+                Debug.Log("4");
+                PointD = Instantiate(GameObject.Find("LocationManager").transform.GetChild(0).gameObject);
+                PointD.SetActive(true);
+                PointD.transform.position = hitInfo.point;
+                PointD.transform.parent = GameObject.Find("LocationManager").transform;
+                //RecordAllPoints(thismdll);
+                GameObject.Find("Button_Location").transform.GetChild(5).gameObject.SetActive(false);
+                CaculateAndRotate();
+
+                //RecordAllPoints(thismdll);
+                Debug.Log(ModelMaganer.mdlobjList);
+                ModelMaganer.Loadobj = 2;
+
+                return;
+            }
+        }
+    }
+
+    public static void CaculateAndRotate()
+    {
+        Vba = PointA.transform.position - PointB.transform.position;
+        //Vab[1] = 0;
+        PointB.transform.position = PointB.transform.position + Vba;
+        PointD.transform.position = PointD.transform.position + Vba;
+        Vac = PointC.transform.position - PointA.transform.position;
+        Vbd = PointD.transform.position - PointB.transform.position;
+        Vac.y = 0;
+        Vbd.y = 0;
+        Debug.Log(Vac + " , " + Vbd);
+        angle = Vector3.Angle(Vbd, Vac);
+        GameObject ModelBox = new GameObject();
+        ModelBox.transform.position = PointA.transform.position;
+        GameObject BIM = GameObject.Find("ModelManager");
+        BIM.transform.parent = ModelBox.transform;
+        BIM.transform.position = BIM.transform.position + Vba;
+        Vector3 bdcrossac = Vector3.Cross(Vbd, Vac);
+        float Crossresult = Vector3.Dot(bdcrossac, Vector3.up);
+        Debug.Log(Crossresult);
+        if (Crossresult >= 0)
+        {
+            ModelBox.transform.eulerAngles = new Vector3(ModelBox.transform.eulerAngles.x, ModelBox.transform.eulerAngles.y + angle, ModelBox.transform.eulerAngles.z);
+        }
+        else
+        {
+            ModelBox.transform.eulerAngles = new Vector3(ModelBox.transform.eulerAngles.x, ModelBox.transform.eulerAngles.y - angle, ModelBox.transform.eulerAngles.z);
+        }
+        BIM.transform.parent = null;
+        CleanAllPoints();
+
+
+
+        GameObject.Find("Button_Location").SendMessage("Locater");
+        GameObject.Destroy(ModelBox);               
+    }
+
+    public static void CleanAllPoints()
+    {
+        foreach (GameObject CountBall in GameObject.FindGameObjectsWithTag("LocatePoint"))
+        {
+            Destroy(CountBall);
+        }
+        Points = 1;
+
+    }
+
+    public static void openmesh(GameObject abc)
+    {
+        if (null == abc)
+            return;
+        foreach (Transform child in abc.transform)
+        {
+            if (null == abc)
+                continue;
+            if (child.gameObject.GetComponent<MeshCollider>() == true)
+                {
+                child.gameObject.GetComponent<MeshCollider>().enabled = true;
+                }
+            openmesh(child.gameObject);
+        }
+    }
+    public static void openrenderer(GameObject abc)
+    {
+        if (null == abc)
+            return;
+        foreach (Transform child in abc.transform)
+        {
+            if (null == abc)
+                continue;
+            if (child.gameObject.GetComponent<MeshRenderer>() == true)
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+            openrenderer(child.gameObject);
+        }
+    }
+    public static void closemesh(GameObject abc)
+    {
+        if (null == abc)
+            return;
+        foreach (Transform child in abc.transform)
+        {
+            if (null == abc)
+                continue;
+            if (child.gameObject.GetComponent<MeshCollider>() == true)
+            {
+                child.gameObject.GetComponent<MeshCollider>().enabled = false;
+            }
+            closemesh(child.gameObject);
+        }
+
+
+    }
+    public static void closerenderer(GameObject abc)
+    {
+        if (null == abc)
+            return;
+        foreach (Transform child in abc.transform)
+        {
+            if (null == abc)
+                continue;
+            if (child.gameObject.GetComponent<MeshRenderer>() == true)
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            closerenderer(child.gameObject);
+        }
+
+
+    }
+    public void Locater()
+    {
+        if (Location.Equals(false))
+        {
+            //transform.GetChild(0).GetComponent<MeshRenderer>().material.color = new Color(0.6f, 0.6f, 0.6f, 0.6f);
+            //LuisManager.instance.LUISFeedback.text = "LUIS:OK,I already turn on Location Function";
+            Location = true;
+            GameObject.Find("MainCamera").transform.GetChild(0).gameObject.SetActive(true);
+            GameObject.Find("Button_Location").transform.GetChild(5).gameObject.SetActive(true);
+            closerenderer(thismdll);
+            foreach (Transform child in thismdll.transform)
+            {
+
+                foreach (Transform childin in child.transform)
+                {
+                    if (childin.gameObject.GetComponent<MeshCollider>() ==true)
+                    {
+                        childin.gameObject.GetComponent<MeshCollider>().enabled = false;
+                    }
+                }
+            }
+            return;
+        }
+        if (Location.Equals(true))
+        {
+            //transform.GetChild(0).GetComponent<MeshRenderer>().material = GameObject.Find("Material").transform.GetChild(0).GetComponent<MeshRenderer>().material;
+            Set_Model.CleanAllPoints();
+            GameObject.Find("MainCamera").transform.GetChild(0).gameObject.SetActive(false);
+            //LuisManager.instance.LUISFeedback.text = "The Location Function is off";
+            GameObject.Find("Button_Location").transform.GetChild(5).gameObject.SetActive(false);
+            Location = false;
+            return;
+        }
+    }
+    public void Update()
+    {
+        if (Location.Equals(true))
+        {
+            if (Physics.Raycast(
+               Camera.main.transform.position,
+               Camera.main.transform.forward,
+               out hitInf,
+               20.0f,
+               Physics.DefaultRaycastLayers))
+            {
+                GameObject.Find("MainCamera").transform.GetChild(0).gameObject.transform.position = hitInf.point;
+                distance = (Camera.main.transform.position - hitInf.point).magnitude;
+                if (distance < 0.8f)
+                {
+                    GameObject.Find("MainCamera").transform.GetChild(0).gameObject.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                }
+                else if (distance > 0.8f)
+                {
+                    GameObject.Find("MainCamera").transform.GetChild(0).gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                }
+            }
+        }
+    }
+}
